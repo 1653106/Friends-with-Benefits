73d96002fb5dc98cf66d0d50fc4988b690e12b83
TOẢN ĐINH TRẦN <1653092@student.hcmus.edu.vn>
2019-04-29 15:03:23 +0700

    Done admin account
    @1653092

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,9 @@
 let adminController = {};
 let models = require('../models');
+let sequelize = require('sequelize');
 let multer = require('multer');
 let User = models.User;
+const Op = sequelize.Op;
 
 adminController.getByID = (req, res, next) => {
     User.findOne({
@@ -117,10 +119,98 @@ adminController.getGender = function(req,res)
 //Load account
 adminController.loadAccount = function (req,res) {
     User.findAll({
-    }).then(User=>{
-        res.locals.account=User;
+        where:{
+            username: {[Op.not] : "admin"}
+        }
+
+    }).then(user=>{
+        user.forEach(element=>{
+            if(element['gender'].trim()=='l')
+            {
+                element['gender']="LGBT";
+            }
+            else
+                if(element['gender'].trim()=='f')
+                {
+                    element['gender']="Female";
+                }
+                else
+                    if(element['gender'].trim()=='m')
+                    {
+                        element['gender']="Male";
+                    }
+            switch(element['city'].trim())
+            {
+                case "hanoi": element['city']="Hà Nội";break;
+                case "tphcm": element['city']="TP HCM";break;
+                case "haiphong": element['city']="Hải Phòng";break;
+                case "danang": element['city']="Đà Nẵng";break;
+                case "cantho": element['city']="Cần Thơ";break;
+                case "angiang": element['city']="An Giang";break;
+                case "bariavungtau": element['city']="Bà Rịa-Vũng Tàu";break;
+                case "bacgiang": element['city']="Bắc Giang";break;
+                case "backan": element['city']="Bắc Kạn";break;
+                case "baclieu": element['city']="Bạc Liêu";break;
+                case "bacninh": element['city']="Bắc Ninh";break;
+                case "bentre": element['city']="Bến Tre";break;
+                case "binhdinh": element['city']="Bình Định";break;
+                case "binhduong": element['city']="Bình Dương";break;
+                case "binhphuoc": element['city']="Bình Phước";break;
+                case "binhthuan": element['city']="Bình Thuận";break;
+                case "camau": element['city']="Cà Mau";break;
+                case "caobang": element['city']="Cao Bằng";break;
+                case "daklak": element['city']="Đắk Lắk";break;
+                case "daknong": element['city']="Đắk Nông";break;
+                case "dienbien": element['city']="Điện Biên";break;
+                case "dongnai": element['city']="Đồng Nai";break;
+                case "dongthap": element['city']="Đồng Tháp";break;
+                case "gialai": element['city']="Gia Lai";break;
+                case "hagiang": element['city']="Hà Giang";break;
+                case "hanam": element['city']="Hà Nam";break;
+                case "hatinh": element['city']="Hà Tĩnh";break;
+                case "haiduong": element['city']="Hải Dương";break;
+                case "haugiang": element['city']="Hậu Giang";break;
+                case "hoabinh": element['city']="Hoà Bình";break;
+                case "hungyen": element['city']="Hưng Yên";break;
+                case "khanhhoa": element['city']="Khánh Hoà";break;
+                case "kiengiang": element['city']="Kiên Giang";break;
+                case "kontum": element['city']="Kon Tum";break;
+                case "laichau": element['city']="Lai Châu";break;
+                case "lamdong": element['city']="Lâm Đồng";break;
+                case "langson": element['city']="Lạng Sơn";break;
+                case "laocai": element['city']="Lào Cai";break;
+                case "longan": element['city']="Long An";break;
+                case "namdinh": element['city']="Nam Định";break;
+                case "nghean": element['city']="Nghệ An";break;
+                case "ninhbinh": element['city']="Ninh Bình";break;
+                case "ninhthuan": element['city']="Ninh Thuận";break;
+                case "phutho": element['city']="Phú Thọ";break;
+                case "quangbinh": element['city']="Quảng Bình";break;
+                case "quangnam": element['city']="Quảng Nam";break;
+                case "quangngai": element['city']="Quảng Ngãi";break;
+                case "quangninh": element['city']="Quảng Ninh";break;
+                case "quangtri": element['city']="Quảng Trị";break;
+                case "soctrang": element['city']="Sóc Trăng";break;
+                case "sonla": element['city']="Sơn La";break;
+                case "tayninh": element['city']="Tây Ninh";break;
+                case "thaibinh": element['city']="Thái Bình";break;
+                case "thainguyen": element['city']="Thái Nguyên";break;
+                case "thanhhoa": element['city']="Thanh Hoá";break;
+                case "thuathienhue": element['city']="Thừa Thiên Huế";break;
+                case "tiengiang": element['city']="Tiền Giang";break;
+                case "travinh": element['city']="Trà Vinh";break;
+                case "tuyenquang": element['city']="Tuyên Quang";break;
+                case "vinhlong": element['city']="Vĩnh Long";break;
+                case "vinhphuc": element['city']="Vĩnh Phúc";break;
+                case "yenbai": element['city']="Yên Bái";break;
+                case "phuyen": element['city']="Phú Yên";break;
+            }
+        })
+        res.locals.account=user;
         res.render('admin-account');
         req.session.current_url = '/login-admin/admin-account';
     })
 }
+//User-chart
+
 module.exports = adminController;
