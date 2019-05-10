ae9ed386e8fb553191e2c2b2de94745f059ac6ca
HSIAO <1653106@student.hcmus.edu.vn>
2019-05-10 17:01:12 +0700

    fix upload image

--- public/js/check-img.js
+++ public/js/check-img.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     var result;
     $('#btnSubmit').click(function(event) {
         if (document.getElementById("choosefile").files.length == 0) {
-            document.getElementById('validate').innerHTML = `Hãy chọn file ảnh trước khi upload`;
+            document.getElementById('validate').innerHTML = `Choose file`;
             result = false;
         } else if (result == true) {
             document.getElementById('validate').style.color = "green";
-            document.getElementById('validate').innerHTML = `Upload thành công`;
+            document.getElementById('validate').innerHTML = `Successfully Uploaded`;
             result = true;
         }
         return result;
@@ -20,7 +20,7 @@ $(document).ready(function() {
             var validImageTypes = ["image/gif", "image/jpeg", "image/png"];
             if ($.inArray(fileType, validImageTypes) < 0) {
                 document.getElementById('validate').style.color = "red";
-                document.getElementById('validate').innerHTML = `File không phải hình ảnh`;
+                document.getElementById('validate').innerHTML = `Please choose image file`;
                 result = false;
                 break;
             }
